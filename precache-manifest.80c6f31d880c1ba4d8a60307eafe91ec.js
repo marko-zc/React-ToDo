fba5793ed3c8e1f63973ddce78f3080d10d353e7
marko-zc <marko.zarkovic.cerneli@gmail.com>
2021-03-08 11:38:56 +0100

    Updates

---
+++ precache-manifest.80c6f31d880c1ba4d8a60307eafe91ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dd4637738abef115389a734128d6db",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "9c883f6595e25434ce87",
+    "url": "/React-ToDo/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d88c364ec58936e4a134",
+    "url": "/React-ToDo/static/css/main.6b05daaa.chunk.css"
+  },
+  {
+    "revision": "9c883f6595e25434ce87",
+    "url": "/React-ToDo/static/js/2.1b0fd059.chunk.js"
+  },
+  {
+    "revision": "07c4bde250ee87a4e437c08a21d91531",
+    "url": "/React-ToDo/static/js/2.1b0fd059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88c364ec58936e4a134",
+    "url": "/React-ToDo/static/js/main.98113bd0.chunk.js"
+  },
+  {
+    "revision": "f4859967934cf8b01825",
+    "url": "/React-ToDo/static/js/runtime-main.beebf042.js"
+  }
+]);
